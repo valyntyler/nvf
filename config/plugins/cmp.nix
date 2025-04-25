6e9c8d73c1f13116e0ba93c301347d89854a8d95
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-25 03:16:09 +0300

    feat: update cmp.nix

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,3 +1,11 @@
 {
-  config.vim.autocomplete.nvim-cmp.enable = true;
+  config.vim.autocomplete.nvim-cmp = {
+    enable = true;
+    mappings = {
+      next = "<C-j>";
+      previous = "<C-k>";
+      scrollDocsDown = "<C-l>";
+      scrollDocsUp = "<C-h>";
+    };
+  };
 }
