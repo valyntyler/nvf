dddddf2dd7d008f94b4261ef71dce99d15a39c3f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-25 03:04:12 +0300

    feat: create conform.nix

---
+++ config/plugins/conform.nix
@@ -0,0 +1,8 @@
+{
+  config.vim.formatter.conform-nvim = {
+    setupOpts = {
+      format_after_save = null;
+      format_on_save = null;
+    };
+  };
+}
--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./cmp.nix
+    ./conform.nix
     ./lsp.nix
     ./luasnip.nix
     ./oil.nix
