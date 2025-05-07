9388c4a050fcde2e29260d34bc09c57e9796841c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-07 20:05:44 +0300

    fix: update oil.nvim opts

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -3,10 +3,10 @@
     enable = true;
     setupOpts = {
       win_options.wrap = true;
-      delete_to_trash = "false";
+      delete_to_trash = "true";
       skip_confirm_for_simple_edits = "false";
       watch_for_changes = "true";
-      view_options.show_hidden = true;
+      view_options.show_hidden = false;
     };
   };
 }
