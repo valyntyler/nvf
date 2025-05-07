417ceb55b3b9f1546005d40a5ff9159d2efca596
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-07 15:14:25 +0300

    feat: update oil.nvim config

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,6 +1,12 @@
 {
   config.vim.utility.oil-nvim = {
     enable = true;
-    setupOpts.skip_confirm_for_simple_edits = "false";
+    setupOpts = {
+      win_options.wrap = true;
+      delete_to_trash = "false";
+      skip_confirm_for_simple_edits = "false";
+      watch_for_changes = "true";
+      view_options.show_hidden = true;
+    };
   };
 }
