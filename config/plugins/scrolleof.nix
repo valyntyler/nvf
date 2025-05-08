7878faa0ccbf13556856496235b7ea5c5459860c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-08 16:58:21 +0300

    fix: disable scrolleof for alpha.nvim buffers

--- config/plugins/scrolleof.nix
+++ config/plugins/scrolleof.nix
@@ -4,7 +4,15 @@
   config.vim.extraPlugins = {
     scrolleof = {
       package = pkgs.vimPlugins.scrolleof;
-      setup = ''require('scrollEOF').setup()'';
+      setup = "
+        require('scrollEOF').setup({
+          pattern = '*',
+          insert_mode = false,
+          floating = false,
+          disabled_filetypes = { '' },
+          disabled_modes = {},
+        })
+      ";
     };
   };
 }
