207bbfe608925cb1c6bedf96ab4a971aedaf4697
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-11 02:49:00 +0300

    fix: vim-tmux binds

--- config/plugins/vim-tmux.nix
+++ config/plugins/vim-tmux.nix
@@ -2,6 +2,14 @@
   config.vim.extraPlugins = {
     vim-tmux-navigator = {
       package = pkgs.vimPlugins.vim-tmux-navigator;
+      setup = ''
+        vim.g.tmux_navigator_no_mappings = 1
+
+        vim.keymap.set('n', '<a-h>', '<cmd>TmuxNavigateLeft<CR>', { silent = true })
+        vim.keymap.set('n', '<a-j>', '<cmd>TmuxNavigateDown<CR>', { silent = true })
+        vim.keymap.set('n', '<a-k>', '<cmd>TmuxNavigateUp<CR>', { silent = true })
+        vim.keymap.set('n', '<a-l>', '<cmd>TmuxNavigateRight<CR>', { silent = true })
+      '';
     };
   };
 }
