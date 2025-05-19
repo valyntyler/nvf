b48b9ebaf122bb76c4d21a846b48f6dc58ed5a60
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-19 21:20:44 +0300

    feat: harpoon navigation binds

--- config/plugins/harpoon.nix
+++ config/plugins/harpoon.nix
@@ -1,3 +1,13 @@
 {
-  config.vim.navigation.harpoon.enable = true;
+  config.vim.navigation.harpoon = {
+    enable = true;
+    mappings = {
+      file1 = "<Leader>hh";
+      file2 = "<Leader>hj";
+      file3 = "<Leader>hk";
+      file4 = "<Leader>hl";
+      listMarks = "<Leader>hq";
+      markFile = "<Leader>ha";
+    };
+  };
 }
