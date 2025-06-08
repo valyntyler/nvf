c1029e0b6e67f6cc1acd776e8b38c49cb5238901
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-08 19:47:03 +0300

    feat: update godot.nix

--- config/plugins/godot.nix
+++ config/plugins/godot.nix
@@ -4,4 +4,7 @@
       package = pkgs.vimPlugins.vim-godot;
     };
   };
+  config.vim.luaConfigRC.vim-godot = ''
+    require'lspconfig'.gdscript.setup{capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())}
+  '';
 }
