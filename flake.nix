29d270e45d9d25cf74f863910950c8942f448667
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-28 15:49:46 +0300

    fix: system dependent package declaration

--- flake.nix
+++ flake.nix
@@ -2,20 +2,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.x86_64-linux.default =
-      (inputs.nvf.lib.neovimConfiguration {
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [(import ./overlay/scrolleof.nix)];
-        };
-        modules = [
-          ./config
-          ./modules
-        ];
-      })
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import ./overlay/scrolleof.nix)];
+      };
+    in {
+      packages.default =
+        (inputs.nvf.lib.neovimConfiguration {
+          inherit pkgs;
+          modules = [
+            ./config
+            ./modules
+          ];
+        })
       .neovim;
-  };
+    });
 }
