0cb98f6fc0b36d9d9897bd2d9e29efb78858b06e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-11 00:43:31 +0300

    feat: html lsp letsgoooo

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,8 +1,17 @@
-{
+{pkgs, ...}: {
   vim.lsp = {
     enable = true;
     formatOnSave = true;
     lspkind.enable = true;
     trouble.enable = true;
+  };
+  vim.lsp.servers = {
+    html-lsp = {
+      enable = true;
+      cmd = ["${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server" "--stdio"];
+      init_options.provideFormatter = true;
+      filetypes = ["html"];
+      root_markers = ["index.html"];
+    };
   };
 }
