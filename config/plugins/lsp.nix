76e25d0e62192d7328ad5a73af0eb800f24c49f6
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-11 15:53:39 +0300

    chore: update lsp.nix

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -9,13 +9,13 @@
     otter-nvim.enable = true;
   };
   vim.lsp.servers = {
-    html = {
+    html-lsp = {
       enable = true;
       cmd = ["${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server" "--stdio"];
       init_options.provideFormatter = true;
       filetypes = ["html"];
     };
-    emmet = {
+    emmet-ls = {
       enable = true;
       cmd = ["${pkgs.emmet-ls}/bin/emmet-ls" "--stdio"];
       filetypes = [
