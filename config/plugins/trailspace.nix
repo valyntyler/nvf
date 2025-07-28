c6670fcc516bfab368a88abb57306df86d11072e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-28 19:55:42 +0300

    feat: update trailspace.nix

--- config/plugins/trailspace.nix
+++ config/plugins/trailspace.nix
@@ -1,3 +1,14 @@
-{
+{lib, ...}: {
   vim.mini.trailspace.enable = true;
+  vim.autocmds = [
+    {
+      enable = true;
+      event = ["BufWritePre"];
+      callback = lib.generators.mkLuaInline ''
+        function()
+          require('mini.trailspace').trim()
+        end
+      '';
+    }
+  ];
 }
