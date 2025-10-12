a103d273ea5565bb62f1228260138ec99a0243d1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-12 21:00:06 +0200

    fix: disable sql diagnostics

--- config/options/languages.nix
+++ config/options/languages.nix
@@ -37,6 +37,7 @@
     sql = {
       enable = true;
       format.enable = false;
+      extraDiagnostics.enable = false;
       lsp = {
         enable = false;
         package = ["${pkgs.sqls}/bin/sqls"];
