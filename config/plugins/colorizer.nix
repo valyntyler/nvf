b97246e6053af0f87fa75127bc6feffeb028d56a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-02 23:18:53 +0100

    feat: update colorizer.nix

--- config/plugins/colorizer.nix
+++ config/plugins/colorizer.nix
@@ -1,3 +1,18 @@
 {
-  vim.ui.colorizer.enable = true;
+  vim.ui.colorizer = {
+    enable = true;
+    setupOpts.filetypes = {
+      css = {
+        RGB = true;
+        RRGGBB = true;
+        RRGGBBAA = true;
+        names = true;
+        rgb_fn = false;
+        hsl_fn = false;
+        css_fn = true;
+        css = true;
+        virtualtext = " ";
+      };
+    };
+  };
 }
