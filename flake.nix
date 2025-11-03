1cab3ac187f53ae5f622000d12c965e99a42d8ec
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-03 19:50:11 +0200

    feat: pinned nvf to my fork

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf/v0.8";
+    nvf.url = "github:valyntyler/fork-nvf/v0.8";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
