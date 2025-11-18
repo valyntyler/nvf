933a8ce6a5d08e31e8ac5174c9ceea4c8492fcd4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-18 01:02:10 +0100

    temp: enable mason

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   vim.lsp = {
     enable = true;
     formatOnSave = true;
@@ -8,4 +8,13 @@
     trouble.enable = true;
     otter-nvim.enable = true;
   };
+
+  vim.extraPlugins.mason-nvim = {
+    package = pkgs.vimPlugins.mason-nvim;
+    setup = ''require("mason").setup()'';
+  };
+
+  vim.luaConfigRC.vue-language-server = ''
+    require("vue-language-server")
+  '';
 }
