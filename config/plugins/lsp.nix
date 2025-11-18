827d5e207acc5195a7e3311a8b81e45cc0abe21b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-18 23:30:30 +0100

    feat: import vue config

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -15,7 +15,57 @@
   };
 
   vim.luaConfigRC.vue-language-server = ''
-    require("vue-language-server")
+    -- ~/.config/nvim/lua/configs/lspconfig.lua
+    -- Loaded automatically by NvChad
+
+    -- Load defaults i.e lua_lsp
+    -- require("nvchad.configs.lspconfig").defaults()
+
+    local lspconfig = require("lspconfig")
+    -- local nvlsp = require("nvchad.configs.lspconfig")
+
+    -- Dynamically point to the path of @vue/language-server
+    -- which contains @vue/typescript-plugin
+    local vue_typescript_plugin =
+    	vim.fn.expand(vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server")
+
+    -- Set up ts_ls LSP with @vue/typescript-plugin
+    lspconfig.ts_ls.setup({
+    	-- on_attach = nvlsp.on_attach,
+    	-- on_init = nvlsp.on_init,
+    	-- capabilities = nvlsp.capabilities,
+    	init_options = {
+    		plugins = {
+    			{
+    				name = "@vue/typescript-plugin",
+    				location = vue_typescript_plugin,
+    				languages = { "vue" },
+    			},
+    		},
+    	},
+    	filetypes = {
+    		"javascript",
+    		"typescript",
+    		"vue",
+    	},
+    	settings = {
+    		typescript = {
+    			tsserver = {
+    				useSyntaxServer = false,
+    			},
+    			inlayHints = {
+    				includeInlayParameterNameHints = "all",
+    				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+    				includeInlayFunctionParameterTypeHints = true,
+    				includeInlayVariableTypeHints = true,
+    				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+    				includeInlayPropertyDeclarationTypeHints = true,
+    				includeInlayFunctionLikeReturnTypeHints = true,
+    				includeInlayEnumMemberValueHints = true,
+    			},
+    		},
+    	},
+    })
   '';
 
   vim.lsp.servers.vue_ls = {
